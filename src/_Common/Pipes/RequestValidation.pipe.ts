20fddb0809b729b70fb2e68c3ba5b80f52d351bc
LASCode <geimixa123@gmail.com>
2022-10-14 03:17:01 +0300

    make request data pipe returned only one field error [field]: string

--- src/_Common/Pipes/RequestValidation.pipe.ts
+++ src/_Common/Pipes/RequestValidation.pipe.ts
@@ -1,4 +1,4 @@
-import { ArgumentMetadata, BadRequestException, HttpStatus, PipeTransform } from '@nestjs/common';
+import { ArgumentMetadata, HttpStatus, PipeTransform } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
 import { validate } from 'class-validator';
 import { CustomHttpException } from '../Exceptions/CustomHttpException';
@@ -11,7 +11,10 @@ export class RequestValidationPipe implements PipeTransform<any> {
       if (errors.length) {
         const result = errors.reduce((acc, curr) => {
           const { property, constraints } = curr;
-          acc[property] = Object.values(constraints).reverse();
+          // Выдача первой в списке ошибки поля - возвращает [key: fieldName]: string;
+          acc[property] = Object.values(constraints).reverse()[0];
+          // Выдача всего массива ошибок поля - возвращает [key: fieldName]: string[];
+          // acc[property] = Object.values(constraints).reverse();
           return acc;
         }, {})
         throw new CustomHttpException(result, HttpStatus.BAD_REQUEST);
